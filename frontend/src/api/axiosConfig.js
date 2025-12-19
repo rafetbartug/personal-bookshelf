1f7a206c72c160406ec518e06766db2f885addb0
Rafet Bartug <rafetbartug@gmail.com>
2025-12-19 12:44:16 +0300

    domain addr updated

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://personal-bookshelf-theta-orpin.vercel.app/';
 
 const api = axios.create({
     baseURL: baseURL,
