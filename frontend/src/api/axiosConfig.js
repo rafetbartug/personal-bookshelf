4f859e928fd3d9bcbdbd6f4222d7cefdda1f991b
Rafet Bartug <rafetbartug@gmail.com>
2025-12-19 12:33:43 +0300

    axios.config updated

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
 
-// Backend adresimiz
 const api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: baseURL,
 });
 
-// Her isteğe Token'ı otomatik ekle (Interceptor)
+// Interceptor kismi gayet düzgün (Token ekleme)
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
